a70a50d6d3ad9264b7ada5e6bdfe0e7529625f19
Andrew Del Prete <pathsofdesign@gmail.com>
2018-07-15 12:46:43 -0500

    Add a few more tests

--- tests/integration/Counter.spec.js
+++ tests/integration/Counter.spec.js
@@ -7,4 +7,28 @@ describe("Counter.vue", () => {
     Simulate.click(getByText("Increments"));
     expect(getByTestId("count").textContent).toBe("1");
   });
+
+  it("decrements by 1 on click", () => {
+    const { getByTestId, getByText } = render(Counter);
+    Simulate.click(getByText("Decrements"));
+    expect(getByTestId("count").textContent).toBe("-1");
+  });
+
+  it("increments by multiplier", () => {
+    const { getByTestId, getByText } = render(Counter, { props: { multiplier: 4 } });
+    Simulate.click(getByText("Increments"));
+    expect(getByTestId("count").textContent).toBe("4");
+  });
+
+  it("decrements by multiplier", () => {
+    const { getByTestId, getByText } = render(Counter, { props: { multiplier: 4 } });
+    Simulate.click(getByText("Decrements"));
+    expect(getByTestId("count").textContent).toBe("-4");
+  });
+
+  it("resets to 0", () => {
+    const { getByTestId, getByText } = render(Counter);
+    Simulate.click(getByText("Reset"));
+    expect(getByTestId("count").textContent).toBe("0");
+  });
 });
